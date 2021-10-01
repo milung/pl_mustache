87bd48f20d640162ce0f907b0a850bd1aa83050a
Milan Unger <nilan.unger@siemens-healthineers.com>
2021-10-01 14:40:39 +0200

    grammar typos in package manifest

--- pack.pl
+++ pack.pl
@@ -1,5 +1,8 @@
 name(pl_mustache).
 version('1.0.0').
-title('Mustache').
-author('Milan Unger', 'nilan.unger@siemens-healthineers.com').
+title('{{mustache}}').
+author('Milan Unger', 'milan.kvetko@gmail.com').
+packager('Milan Unger', 'milan.kvetko@gmail.com').
+maintainer('Milan Unger', 'milan.kvetko@gmail.com').
 home('https://github.com/milung/pl_mustache.git').
+provides(mustache).
